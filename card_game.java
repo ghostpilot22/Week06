29618821d62a3a1a9ea8b7a9b4f873a23b753a6e
Letitia Pinter <60904782+ghostpilot22@users.noreply.github.com>
2024-02-16 15:03:36 -0700

    Got everything working except for shuffle, draw, and flip

--- card_game.java
+++ card_game.java
@@ -10,10 +10,54 @@ public class App
 {
 	public static void main(String[] args)
 	{
-		Card c = new Card(5, "Diamonds");
-		System.out.println(c.getValue());
-		System.out.println(c.getName());
-		c.describe();
+		Deck d = new Deck();
+		Player p1 = new Player("Player 1");
+		Player p2 = new Player("Player 2");
+		d.shuffle();
+		for(int i = 0; i<26; i++)
+		{
+			p1.draw(d);
+			p2.draw(d);
+		}
+		// The actual game starts now.
+		for(int i = 0; i<26; i++)
+		{
+			Card c1 = p1.flip();
+			System.out.print(p1.getName() + "'s card: ");
+			c1.describe();
+			Card c2 = p2.flip();
+			System.out.print(p2.getName() + "'s card: ");
+			c2.describe();
+			
+			if(c1.getValue() > c2.getValue())
+			{
+				System.out.println(p1.getName() + " gets a point.");
+				p1.incrementScore();
+			}
+			else if(c1.getValue() < c2.getValue())
+			{
+				System.out.println(p2.getName() + " gets a point.");
+				p2.incrementScore();
+			}
+			else // if the cards are equal
+			{
+				System.out.println("No point was awarded.");
+			}
+			System.out.println(p1.getName() + "'s score: " + p1.getScore());
+			System.out.println(p2.getName() + "'s score: " + p2.getScore());
+		}
+		
+		// Once game is over:
+		System.out.println(""); // Spacer
+		System.out.println(p1.getName() + "'s score: " + p1.getScore());
+		System.out.println(p2.getName() + "'s score: " + p2.getScore());
+		
+		if(p1.getScore() > p2.getScore())
+			System.out.println(p1.getName() + " wins!");
+		else if(p1.getScore() < p2.getScore())
+			System.out.println(p2.getName() + " wins!");
+		else
+			System.out.println("Draw");
 	}
 }
 
@@ -23,7 +67,7 @@ public class Card
 	private String name;
 	
 	public Card(int value, String suit)
-	{
+	{ // Constructor. Creates the card and populates the fields.
 		this.value = value;
 		switch(value)
 		{
@@ -55,10 +99,64 @@ public class Card
 
 public class Deck
 {
+	private ArrayList<Card> cards;
 	
+	public Deck()
+	{ // Constructor. Populates the deck with cards.
+		ArrayList<Card> cards = new ArrayList<Card>();
+		
+		for(int i = 2; i <= 14; i++)
+		{
+			cards.add(new Card(i, "Hearts"));
+			cards.add(new Card(i, "Diamonds"));
+			cards.add(new Card(i, "Spades"));
+			cards.add(new Card(i, "Clubs"));
+		}
+		this.cards = cards;
+	}
+	
+	public void shuffle()
+	{
+		
+	}
+	
+	public Card draw()
+	{
+		return cards.get(0); // Temporary to get it to run.
+		// Fix this to make it pop the card off the deck.
+	}
 }
 
 public class Player
 {
+	private ArrayList<Card> hand;
+	private int score;
+	private String name;
 	
+	public Player(String name)
+	{
+		this.name = name;
+		this.score = 0;
+		this.hand = new ArrayList<Card>();
+	}
+	
+	public void describe()
+	{
+		System.out.println(name + ", Score: " + score + ", Hand:");
+		for(Card c : hand) c.describe();
+	}
+	
+	public Card flip()
+	{
+		return hand.get(0); // Again need to fix
+	}
+	
+	public void draw(Deck deck)
+	{
+		hand.add(deck.draw());
+	}
+	
+	public void incrementScore() { score++; }
+	public int getScore() { return score; }
+	public String getName() { return name; }
 }
